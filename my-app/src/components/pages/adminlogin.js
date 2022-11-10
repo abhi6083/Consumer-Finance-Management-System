0d01d280ad3cc5a5c837c930c1a36620e2da46ef
Abhishek Gupta <60040591+abhi6083@users.noreply.github.com>
2022-11-10 13:31:48 +0530

    added admin login with backend

--- my-app/src/components/pages/adminlogin.js
+++ my-app/src/components/pages/adminlogin.js
@@ -1,9 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 import '../../App.css'
 
 export default function AdminLoginPage() {
+
+    const [uname, setuname] = useState()
+    const [pass, setpass] = useState()
+    const navigate = useNavigate();
+
+    const submit = () => {
+        fetch('http://localhost:9797/adminRest/api/validate/' + uname + '/' + pass, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                if (JSON.stringify(response) === '1') {
+                    alert("Admin Doesn't exist")
+                }
+                else if (JSON.stringify(response) === '2') {
+                    // fetch('http://localhost:9797/userRest/api/userfind/' + uname, {
+                    // }).then(response => response.json())
+                    //     .then(response => {
+                            console.log("Correct password")
+                            navigate("/adminpage")
+                        }
+                else {
+                    console.log(JSON.stringify(response))
+                    alert("Wrong Credentials")
+                }
+            }
+            )
+    }
+
+
     return (
 
         <section id="body">
@@ -12,20 +46,17 @@ export default function AdminLoginPage() {
                 <div className="container" >
                     <div>
                         <label for="username"><b>Admin Name</b></label>
-                        <input type="text" name="username" placeholder="Enter AdminName" required /><br />
+                        <input type="text" name="username" placeholder="Enter AdminName" onChange={(e)=>setuname(e.target.value)} required /><br />
                     </div>
                     <br />
                     <div>
                         <label for="userpass"><b>Password</b></label>
-                        <input type="password" name="userpass" placeholder="Enter Password" required /> <br />
+                        <input type="password" name="userpass" placeholder="Enter Password" onChange={(e)=>setpass(e.target.value)} required /> <br />
                     </div>
                     <br />
                     <div style={{ display: "flex" }}>
-                        <button type="submit" value="Login"> LOGIN</button>
+                        <button type="submit" value="Login" onClick={submit}>LOGIN</button>
                     </div>
-                </div>
-                <div className="alert-danger">
-                    <span><b>Message</b> </span>
                 </div>
             </form>
         </section>
